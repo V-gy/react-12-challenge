8295ff1157a9ff61f27ea334540d64b197927f80
V-gy <vgy@hotmail.fr>
2019-11-30 23:27:27 +0100

    Updates

---
+++ precache-manifest.3cf4799f5321fbfbaf0d8555bda85432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d819d2d221893a2dac974256814859e6",
+    "url": "/react-12-challenge/index.html"
+  },
+  {
+    "revision": "c2948c3422b0ffde8d28",
+    "url": "/react-12-challenge/static/css/main.656f0744.chunk.css"
+  },
+  {
+    "revision": "236d0d455152fa9dc9e8",
+    "url": "/react-12-challenge/static/js/2.100ed712.chunk.js"
+  },
+  {
+    "revision": "c2948c3422b0ffde8d28",
+    "url": "/react-12-challenge/static/js/main.9fa47c23.chunk.js"
+  },
+  {
+    "revision": "6c55ad4a5f1cd2ae4232",
+    "url": "/react-12-challenge/static/js/runtime-main.14da89bf.js"
+  }
+]);
